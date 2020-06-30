0738680d4d4c32ee04a06f640e87bf94e475868e
Hadassah Dobner <hadassah0987@gmail.com>
2020-06-29 23:14:57 -0400

    salary

--- myfirst.py
+++ myfirst.py
@@ -1,1 +1,14 @@
-print("hello world")
+def greaterOf(val1, val2):
+    if val1 > val2 : 
+        return val1
+    else:
+        return val2
+
+h = float(input("Enter Hours:"))
+rate = input("Enter hour per rate:")
+rt = float(rate)
+total = h * rt
+overtimeHrs = greaterOf(h - 40, 0)
+total += overtimeHrs * rt * .5
+print(total)    
+    
